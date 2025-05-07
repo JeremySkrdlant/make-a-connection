6e9167c03189e38cd4cdb29147fe7a2b0c31c0fc
Jeremy Skrdlant <jeremy.skrdlant@nwktc.edu>
2025-05-07 13:52:56 -0600

    added contract

---
+++ hardhat/contracts/simple.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.27;
+
+contract BaseContract {
+    string public contractCreator = "Jeremy Skrdlant"; 
+
+    
+}
